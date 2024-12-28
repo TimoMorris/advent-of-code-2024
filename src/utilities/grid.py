95f07a1bc315d661b34b6aa58706bee2e68265bc
Tim Morris <35537687+TimoMorris@users.noreply.github.com>
2024-12-28 11:18:02 +0000

    Fix type hints and other small refactors of Grid.

--- src/utilities/grid.py
+++ src/utilities/grid.py
@@ -3,7 +3,7 @@
 Methods supporting finding neighbouring cells and getting all cells in the grid matching certain criteria.
 """
 
-from typing import TypeAlias, Iterable, Callable, Literal, TypeVar
+from typing import Self, Type, TypeAlias, Iterable, Callable, Literal, TypeVar
 
 Coordinates: TypeAlias = tuple[int, int]
 SearchStrategy: TypeAlias = Literal["adjacent", "diagonal", "neighbouring"]
@@ -23,22 +23,22 @@ class Cell[TValue]:
         self.value: TValue = value
         self._grid: "Grid" = grid
 
-    def __add__(self, other: Coordinates) -> "Cell":
+    def __add__(self, other: Coordinates) -> Self:
         row_offset, col_offset = other
         return self._grid.try_get_cell(self.row + row_offset, self.col + col_offset)
 
-    def _get_offsetted_cells(self, offsets: list[Coordinates]) -> Iterable["Cell"]:
+    def _get_offsetted_cells(self, offsets: list[Coordinates]) -> Iterable[Self]:
         for offset in offsets:
             if (offset_cell := self + offset) is not None:
                 yield offset_cell
 
-    def get_adjacent_cells(self):
+    def get_adjacent_cells(self) -> Iterable[Self]:
         return self._get_offsetted_cells(STRAIGHT_VECTORS)
 
-    def get_diagonal_cells(self):
+    def get_diagonal_cells(self) -> Iterable[Self]:
         return self._get_offsetted_cells(DIAGONAL_VECTORS)
 
-    def get_neighboring_cells(self):
+    def get_neighboring_cells(self) -> Iterable[Self]:
         return self._get_offsetted_cells(ALL_VECTORS)
 
 
@@ -53,10 +53,11 @@ class Grid[TCell]:
         self._cols: list[list[TCell]] = [[] for __ in range(self.width)]
         self._cells: dict[Coordinates, TCell] = {}
 
-    @staticmethod
-    def from_lists(lists: list[list[int | str]], cell_class: TCell = Cell) -> "Grid":
+    @classmethod
+    def from_lists(cls, lists: Iterable[Iterable[int | str]], cell_class: Type[TCell] = Cell) -> "Grid":  # return type should be Self but bug in PyCharm
         """Create a grid from a list of lists"""
-        grid = Grid(len(lists), len(lists[0]))
+        lists = [[x for x in list_] for list_ in lists]
+        grid = cls(len(lists), len(lists[0]))
         for row, list_ in enumerate(lists):
             for col, item in enumerate(list_):
                 cell = cell_class(row, col, item, grid=grid)
@@ -70,22 +71,10 @@ class Grid[TCell]:
 
         return grid
 
-    @staticmethod
-    def from_strings(strings: list[str], cell_class: TCell = Cell) -> "Grid":
+    @classmethod
+    def from_strings(cls, strings: Iterable[str], cell_class: Type[TCell] = Cell) -> "Grid":  # return type should be Self but bug in PyCharm
         """Create a grid from a list of strings."""
-        grid = Grid(len(strings), len(strings[0]))
-        for row, str_ in enumerate(strings):
-            for col, char in enumerate(str_):
-                cell = cell_class(row, col, char, grid=grid)
-                grid._cells[row, col] = cell
-                grid._rows[row].append(cell)
-                grid._cols[col].append(cell)
-
-        assert len(grid._cells) == grid.height * grid.width
-        assert all(len(row) == grid.width for row in grid._rows)
-        assert all(len(col) == grid.height for col in grid._cols)
-
-        return grid
+        return cls.from_lists(strings, cell_class=cell_class)
 
     def try_get_cell(self, row, col):
         """Fetch the cell at the given row and column if it exists."""
@@ -103,8 +92,8 @@ class Grid[TCell]:
         starting_cells: list[TCell],
         can_connect: Callable[[TCell, TCell], bool],
         connect: Callable[[TCell, TCell], None],
-        *,
-        search: SearchStrategy = "adjacent"
+        # *,
+        # search: SearchStrategy = "adjacent"
     ) -> None:
         """Recursively loop over neighbouring cells, connecting those matching the condition.
 
